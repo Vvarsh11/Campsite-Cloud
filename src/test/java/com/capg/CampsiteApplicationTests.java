932b448fc31d9d9d73b8bf9f544109323f8454ef
Varshitha <revuri.varshitha@capgemini.com>
2023-02-25 17:56:15 +0530

    Adding project phase

---
+++ src/test/java/com/capg/CampsiteApplicationTests.java
@@ -0,0 +1,12 @@
+package com.capg;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampsiteApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+//
+}
